11e91fd3459e53c6fb1ab936db8bf4531d604459
Julien Genestoux <julien.genestoux@gmail.com>
2023-01-27 20:40:14 -0500

    commented wine tasting lock

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,11 +21,11 @@ export default function Home() {
           emailRequired: true,
           password: true,
         },
-        "0xe204a6976676097631ec65d26500acbd445ad3f4": {
-          network: 100,
-          emailRequired: true,
-          password: true,
-        },
+        // "0xe204a6976676097631ec65d26500acbd445ad3f4": {
+        //   network: 100,
+        //   emailRequired: true,
+        //   password: true,
+        // },
       },
       pessimistic: true,
     };
